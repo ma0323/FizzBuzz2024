97a256e0b0840387b3a3e57dc1fe8616e451b04e
ma0323 <m25593434@gmail.com>
2024-10-04 15:07:33 +0900

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+number = int(input("いくつまで数えますか:"))
+
+for a in range (1,number +1):
+    if a % 3 == 0 and a % 5 == 0:
+        print("FizzBuzz")
+
+    elif a % 3 == 0:
+        print("Fizz")
+
+    elif a % 5 == 0 :
+        print ("Buzz")
+
+    else:
+     print (a)
